dfdf023a2f3c063974d5495781894771a9875716
simplycodepix <simplycodepix@gmail.com>
2020-12-16 11:22:40 +0200

    update bergamot config + readme

--- bergamot.config.js
+++ bergamot.config.js
@@ -2,13 +2,13 @@ let js_transform = (js) => js.replace(/([>`}])(\s*\n\s*)([<`\$])/mg,"$1$3").repl
 
 module.exports = {
     lpcandy: {
-        root_path: "<path_to_root_folder>/lpcandy",
+        root_path: __dirname + "/lpcandy",
         entry_point: "front/site/index.js",
         bundle_path: "public/assets/lpcandy.min.js",
         js_transform
     },
     projects: {
-        root_path: "<path_to_root_folder>/lpcandy",
+        root_path: __dirname + "/lpcandy",
         entry_point: "../src/index.js",
         bundle_path: "../build/projects.min.js",
         js_transform
